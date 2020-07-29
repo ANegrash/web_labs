046e85cadd2fc345eee4e97dc26d392d0efc3f04
Andrey Negrash <andi.ne@yandex.ru>
2020-07-29 14:01:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
-		<div class="header">
+		<div class="header"><!--Создаём шапку страницы-->
 			<h3><center>Неграш Андрей Владимирович</center></h3>
 			<p>
 			    <center>
@@ -19,6 +19,7 @@
 			<hr>
 		</div>
 		<?php
+                    //эта конструкция нужна для того, чтобы добавлять каждую следующую строчку в таблицу, а не заменять
 		    session_start(); 
             if (!isset($_SESSION['counter'])) $_SESSION['counter']=0; 
             $_SESSION['counter']++;
@@ -60,40 +61,43 @@
 		    </center>
 		</div>
 		<?php
-		include 'script.php';
+		include 'script.php'; //определение попадания точки в область происходит здесь. ВАЖНО! в файле .html эта строка не будет работать
 		?>
 		<script>
+			//здесь мы не даём пользователю ввести в поле для Y ввести не те данные
 		function onlyDigits() {
-      this.value = this.value.replace(/[^\d\,\-]/g, "");
+      this.value = this.value.replace(/[^\d\,\-]/g, "");//разрешаем ввод только цифр 0-9, запятой и минуса
 
-          if(this.value.lastIndexOf("-")> 0) {
-            this.value = this.value.substr(0, this.value.lastIndexOf("-"));
+          if(this.value.lastIndexOf("-")> 0) {//если пользователь вводит тире (минус) не самым первым символом...
+            this.value = this.value.substr(0, this.value.lastIndexOf("-"));//то удаляем этот минус
           }
       
-        if(this.value[0]== "-") {
+        if(this.value[0]== "-") {//если первый символ это минус (число отрицательно)...
             if(this.value[1]== "4" || this.value[1]== "5" || this.value[1]== "6" || this.value[1]== "7" || this.value[1]== "8" || this.value[1]== "9"){
-                this.value = this.value.substr(0, 1);
+                this.value = this.value.substr(0, 1);//то запрещаем вводить цифры 4,5,6,7,8,9
             }
-            if(this.value.length>2 && this.value[2]!=",") this.value=this.value[0]+this.value[1]+","+this.value[2];
-            if(this.value.length>8) this.value = this.value.substr(0, 8);
-        }else{
+            if(this.value.length>2 && this.value[2]!=",") this.value=this.value[0]+this.value[1]+","+this.value[2];//если третий символ введён и он не запятая, то вставляем запятую между вторым и третим символом
+            if(this.value.length>8) this.value = this.value.substr(0, 8);//если количество символов равно 8 (5 знаков после запятой), не даём вводить больше
+        }else{//если число положительно (первым введён не минус, а цифра)...
             if(this.value[0]== "6" || this.value[0]== "7" || this.value[0]== "8" || this.value[0]== "9"){
-                this.value = this.value.substr(0, 0);
+                this.value = this.value.substr(0, 0);//то эта цифра должна быть от 0 до 5
             }
-            if(this.value.length>1 && this.value[1]!=",") this.value=this.value[0]+","+this.value[1];
-            if(this.value.length>7) this.value = this.value.substr(0, 7);
+            if(this.value.length>1 && this.value[1]!=",") this.value=this.value[0]+","+this.value[1];//если второй символ введён и он не запятая, то вставляем запятую между первым и вторым символом
+            if(this.value.length>7) this.value = this.value.substr(0, 7);//если количество символов равно 7 (5 знаков после запятой), не даём вводить больше
         }
       
-        if(this.value.match(/\,/g).length > 1) {
+        if(this.value.match(/\,/g).length > 1) {//не даём ввести больше одной запятой
             this.value = this.value.substr(0, this.value.lastIndexOf(","));
         }
-        if(this.value.match(/\-/g).length > 1) {
+        if(this.value.match(/\-/g).length > 1) {//не даём ввести больше одного минуса
             this.value = this.value.substr(0, 0);
         }
     }
+			//присваиваем данный скрипт нашему полю ввода
     document.querySelector(".number").onkeyup = onlyDigits
-            
-            
+            //Есть два косяка: если ввести только запятую и отправить - ошибок не будет, и если ввести число 5,99999 (что явно больше 5), то ошибки не будет. Это несложно изменить, но если вы читаете этот коммент, значит у меня прокатило и я не исправлял)
+            //а не сделал, потому что лень)
+			//следующие строчки я скопипастил со stackoverflow и они не дают выбирать больше одного checkbox-а. Работает - не трогай
             var inputs = document.getElementsByName("coordX");
             for(var i = 0; i < inputs.length; i++) inputs[i].onchange = checkboxHandler;
          
